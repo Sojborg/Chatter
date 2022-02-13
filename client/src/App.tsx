0d782f8aafe6e101bab4176eb0af2a8a3972391a
Sojborg <sojborg@outlook.com>
2022-02-13 14:45:28 +0100

    register user

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import {Chat} from "./Views/Chat/Chat";
 import {Login} from "./Views/Join/Login";
+import {Register} from "./Views/Register/Register";
 
 const App = () => {
   return (
     <Router>
       <Route path="/" exact component={Login}/>
+      <Route path="/register" exact component={Register}/>
       <Route path="/chat" component={Chat}/>
     </Router>
   );
---
+++ client/src/Views/Register/Register.tsx
@@ -0,0 +1,69 @@
+﻿import React, {useState} from 'react';
+
+import './Register.css';
+import {useHistory} from "react-router-dom";
+
+export const Register = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const register = async () => {
+    const response = await fetch('http://localhost:5000/register', {
+      method: 'POST',
+      headers: new Headers(
+        {
+          'Content-Type': 'application/json'
+        }
+      ),
+      body: JSON.stringify({email, password, username})
+    });
+
+    try {
+      if (response.status === 201) {
+        await response.json();
+        history.push('/');
+      }
+    }
+    catch(e) {
+
+    }
+  }
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Register</h1>
+        <div>
+          <input
+            placeholder="Username"
+            className="joinInput"
+            type="text"
+            onChange={(event) => setUsername(event.target.value)}/>
+        </div>
+        <div>
+          <input
+            placeholder="Email"
+            className="joinInput"
+            type="text"
+            onChange={(event) => setEmail(event.target.value)}/>
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className="joinInput mt-20"
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}/>
+        </div>
+
+        <button
+          className={'button mt-20'}
+          onClick={register}
+          type="submit">Register
+        </button>
+
+      </div>
+    </div>
+  );
+}
