4b4fc634e3b3dd27113473233ea31381ba907afe
Sojborg <sojborg@outlook.com>
2022-03-19 08:24:52 +0100

    new index

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,24 @@
-import http from 'http';
-import express from "express" ;
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import {authRoutes} from "./Routes/Auth";
-import {json} from 'body-parser';
-import {chatRoutes} from "./Routes/Chat";
-import {ChatSocket} from "./Sockets/ChatSocket";
-import {usersRoutes} from "./Routes/Users";
-import {channelRoutes} from "./Routes/ChannelRoute";
+import express from "express";
 import path from "path";
 
-const jsonParser = json();
-
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log('DB Connected'))
-  .catch((err) => console.error(err))
-
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(jsonParser);
+
+app.use(express.json());
+// Serve the React static files after build
 const clientDirPath = __dirname + "/../client/build";
 app.use(express.static(clientDirPath));
-app.use(cors());
-app.use(authRoutes);
-app.use('/api/chat', chatRoutes);
-app.use(usersRoutes);
-app.use('/api/channel', channelRoutes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
 
-const PORT = process.env.PORT || 5000;
-const server = http.createServer(app);
+app.get("/api/hello", (req, res) => {
+  res.send({ message: "Hello" });
+});
 
-const chatSocket = new ChatSocket(server);
-
-server.listen(PORT);
+// All other unmatched requests will return the React app
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+});
