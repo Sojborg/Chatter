d20bbffabe849ec7446a7420bb7be1d0b0ff1d33
Sojborg <sojborg@outlook.com>
2022-04-03 00:25:20 +0200

    fixed imports

--- client/src/Views/Chat/Chat.tsx
+++ client/src/Views/Chat/Chat.tsx
@@ -1,10 +1,10 @@
-import React, {useContext} from "react";
+import React from "react";
 import './Chat.css';
 import {Messages} from "../../Components/Messages";
 import {SocketProvider} from "../../Providers/SocketProvider";
 import {RightSide} from "./RightSide/RightSide";
 import {LeftSide} from "./LeftSide/LeftSide";
-import {ChannelsContext, ChannelsProvider} from "../../Providers/ChannelsProvider";
+import {ChannelsProvider} from "../../Providers/ChannelsProvider";
 import {ModalProvider} from "../../Providers/ModalProvider";
 import {TopBar} from "./TopBar";
 
